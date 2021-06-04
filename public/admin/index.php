b49f690fa040d52d1b0ca9304ddbc545163f76aa
Kayque Silva <kayquebrpds@gmail.com>
2021-06-03 23:59:52 -0300

    iniciando parte do cliente

--- public/admin/index.php
+++ public/admin/index.php
@@ -3,13 +3,13 @@ if (isset($_REQUEST["classe"]) && isset($_REQUEST["metodo"])) {
     $classe = $_REQUEST["classe"];
     $metodo = $_REQUEST["metodo"];
 
-    include_once "../app/controller/$classe.php";
+    include_once "../../app/controller/$classe.php";
     $obj = new $classe();
 
     if (method_exists($obj, $metodo))
         $obj->$metodo();
     else
-        include_once "../app/view/PaginaErro.php";
+        include_once "../../app/view/admin/PaginaErro.php";
 } else {
-    header("location:index.php?classe=HomeController&metodo=abrirLoja");
+    header("location:index.php?classe=HomeController&metodo=abrirHome");
 }
